9e1b4f08bfcbbd486b111cec181aaf12088797b3
ChrisOtt0 <sophusott@gmail.com>
2023-01-27 02:00:14 +0100

    rppal test1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{thread, process};
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
 fn main() {
-    println!("Hello, world!");
+    let gpio = match Gpio::new() {
+        Ok(v) => v,
+        _ => {
+            println!("Error creating GPIO struct.");
+            process::exit(1);
+        }
+    };
+
+    let pin = match gpio.get(18) {
+        Ok(v) => v,
+        _ => {
+            println!("Error creating PIN18.");
+            process::exit(1);
+        }
+    };
+    let mut pin = pin.into_output();
+
+    pin.set_high();
+    thread::sleep(Duration::from_secs(2));
+    pin.set_low();
 }
